0a1d7c9f929ab4d0a4c25f25f96bb9fd6c17622b
Anubhav Sharma <anubhav@erponweb.com>
2020-03-04 11:38:35 -0500

    new router change

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e569bd951f1bbffe3ed887025945cb64.js"
+  "/precache-manifest.afa39b2d739fc6005dd42c45be209ccb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
